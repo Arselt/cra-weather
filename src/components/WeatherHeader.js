c1cf1a75bafcc0e81b741a1cf680aea230ba343b
Arath Astorga <arastorga@outlook.com>
2021-12-10 22:52:41 -0600

    🔥 removed hamburger menu

--- src/components/WeatherHeader.js
+++ src/components/WeatherHeader.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Menu } from './menu/Menu';
 
 function WeatherHeader() {
     return(
@@ -7,7 +6,6 @@ function WeatherHeader() {
             <section className="WeatherHeader__Search">
                 <input type="text" placeholder="Search your location"></input>
             </section>
-            <Menu />
         </header>
     );
 }
